4d7048a272ff600ff11367d0ed8ebee1a7796dbf
shencheng <zhangyubo@kungeek.com>
2020-12-11 22:54:18 +0800

    增加第二式

--- src/main/java/zhangyubo_20201211/JianFa.java
+++ src/main/java/zhangyubo_20201211/JianFa.java
@@ -3,6 +3,7 @@ package zhangyubo_20201211;
 public class JianFa {
     public static void main(String[] args) {
         System.out.println("新剑法第一式：欲练此功");
+        System.out.println("新剑法第二式：必先自宫");
     }
 }
 //红色，未提交；绿色，暂存区；黑色，提交到本地库；蓝色，提交到本地库后又修改了
