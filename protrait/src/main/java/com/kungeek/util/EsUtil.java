ad83fda13b30902880331b8301a16d6d1e97bdd0
zhangyubo <1848661150@qq.com>
2020-12-23 23:17:04 +0800

    解决rest客户端连接密码问题

--- protrait/src/main/java/com/kungeek/util/EsUtil.java
+++ protrait/src/main/java/com/kungeek/util/EsUtil.java
@@ -2,22 +2,25 @@ package com.kungeek.util;
 
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.cluster.Health;
 import io.searchbox.core.Index;
 import io.searchbox.core.Search;
 import io.searchbox.core.SearchResult;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.MatchAllQueryBuilder;
-import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.highlight.HighlightBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class EsUtil {
     static JestClientFactory factory = null;
+
+    static protected final Log log = LogFactory.getLog(EsUtil.class.getName());
 
     public static JestClient getClient(){
 
@@ -27,12 +30,13 @@ public class EsUtil {
         return factory.getObject();
     }
 
-    public static void build(){
+    private static void build(){
 
         if(factory == null){
 
             factory = new JestClientFactory();
             factory.setHttpClientConfig(new HttpClientConfig.Builder("http://hxduat.kungeek.com/es/" )
+                    .defaultCredentials("elastic", "123456")
                     .multiThreaded(true)
                     .maxTotalConnection(20)
                     .connTimeout(10000).readTimeout(1000).build());
@@ -52,38 +56,60 @@ public class EsUtil {
         client.close();
     }
 
-    public static String search(String s) throws IOException {
+    public void search() throws IOException {
 
-        SearchSourceBuilder sebu = new SearchSourceBuilder();
-        BoolQueryBuilder bo = new BoolQueryBuilder();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 
-        bo.should(new MatchQueryBuilder("", ""));
+        QueryBuilder queryBuilder = QueryBuilders.termQuery("staff_name", "郭飒飞");
 
-        sebu.query(bo);
+        searchSourceBuilder.query(queryBuilder);
 
-        sebu.from(0);
-        sebu.size(20);
-        sebu.highlight(new HighlightBuilder().field(""));
+        searchSourceBuilder.from(0);
+        searchSourceBuilder.size(20);
+        String query = searchSourceBuilder.toString();
+        System.out.println(query);
 
-        String query2 = sebu.toString();
-        System.out.println(query2);
+        String query01 = "{\n" +
+                "  \"from\" : 0,\n" +
+                "  \"size\" : 20,\n" +
+                "  \"query\" : {\n" +
+                "    \"term\" : {\n" +
+                "      \"staff_name\" : \"郭飒飞\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
 
-        Search search = new Search.Builder(query2).addIndex("").addType("").build();
-
+        Search search = new Search.Builder(query01).addIndex("staff_tag").addType("_doc").build();
         JestClient client = getClient();
         SearchResult result = client.execute(search);
 
-        System.out.println(result);
+        List<SearchResult.Hit<Object, Void>> hits = result.getHits(Object.class);
+
+        System.out.println("Size" + hits.size());
+
+        for (SearchResult.Hit<Object, Void> hit : hits){
+            System.out.println(hit.source.toString());
+        }
 
         client.close();
+    }
 
-        return null;
+    public static JestResult health(){
+        Health health = new Health.Builder().build();
+        JestResult result = null;
+        try {
+            result = getClient().execute(health);
+            log.info("health == " + result.getJsonString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
 
     @Test
     public void test() throws IOException {
-        getClient();
-        search("");
+
     }
 }
